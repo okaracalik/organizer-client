b5f79be1ef0ad8f9c05c515a1c41694b4ad60595
Orkut Karacalik <orkutkaracalik@hotmail.com>
2019-11-07 13:50:21 +0100

    :sparkles: new compoents

---
+++ src/components/Week.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+    <slot name="day" :props="{ starts, ends }"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Week',
+  props: {
+    starts: {
+      type: Date,
+      required: true
+    },
+    ends: {
+      type: Date,
+      required: true
+    }
+  }
+}
+</script>
