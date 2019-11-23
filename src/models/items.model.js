aae09efa5300ea8e21bf67360d1ecd65adaea42d
Orkut Karacalik <orkutkaracalik@hotmail.com>
2019-11-23 18:28:29 +0100

    :sparkles: add item-module

---
+++ src/models/items.model.js
@@ -0,0 +1,45 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function(app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const items = sequelizeClient.define(
+    'items',
+    {
+      pk_items: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {
+      hooks: {
+        beforeCount(options) {
+          options.raw = true;
+        }
+      }
+    }
+  );
+
+  // eslint-disable-next-line no-unused-vars
+  items.associate = function(models) {
+    items.belongsToMany(models.tags, {
+      as: 'tags',
+      through: 'item_tags',
+      foreignKey: 'fk_items'
+    });
+    items.belongsToMany(models.lists, {
+      as: 'lists',
+      through: 'list_items',
+      foreignKey: 'fk_items'
+    });
+  };
+
+  return items;
+};
