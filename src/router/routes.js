dab155c7238534b9684df9cb97c7a8e3bccca1fa
Orkut Karacalik <orkutkaracalik@hotmail.com>
2020-07-13 14:42:12 +0200

    :bug: fix home page

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,7 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', name: 'Index', component: () => import('pages/Index.vue') }
+      { path: '', name: 'Calendar', component: () => import('pages/Calendar.vue') }
     ]
   },
   {
